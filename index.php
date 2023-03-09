d495d47dea70ab513d0056547815c23319dc252f
MATHEUS ZANONI BITTENCOURT DE OLIVEIRA <oliveira.4942@aluno.pr.senac.br>
2023-03-09 10:52:43 -0300

    salva

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+    $pessoa1 = array();
+    $pessoa2 = array();
+    $pessoa3 = array();
+    $pessoa4 = array();
+    
+
+    $pessoa1 [0][0][0][0][0][0] = "Joao";
+    $pessoa1 [0][0][0][0][1][1] = 16;
+    $pessoa1 [0][0][0][0][2][2] = "linguica";
+
+    $pessoa2 [0][0][0][0][0][0] = "Matheus";
+    $pessoa2 [0][0][0][0][1][1] = 15;
+    $pessoa2 [0][0][0][0][2][2] = "girafa";
+
+    $pessoa3 [0][0][0][0][0][0] = "luiz";
+    $pessoa3 [0][0][0][0][1][1] = 16;
+    $pessoa3 [0][0][0][0][2][2] = "chifrudo";
+
+    $pessoa4 [0][0][0][0][0][0] = "xand";
+    $pessoa4 [0][0][0][0][1][1] = 15;
+    $pessoa4 [0][0][0][0][2][2] = "cabeçudo";
+
+    echo " O " . $pessoa1[0][0][0][0][0][0]  . " tem " . $pessoa1[0][0][0][0][1][1] . " e é da casa da " . $pessoa1[0][0][0][0][2][2];
+    echo("</br>");
+    echo " O " . $pessoa2[0][0][0][0][0][0] . " tem " . $pessoa2[0][0][0][0][1][1] . " e é da casa da " . $pessoa2[0][0][0][0][2][2];
+    echo("</br>");
+    echo " O " . $pessoa3[0][0][0][0][0][0] . " tem " . $pessoa3[0][0][0][0][1][1] . " e é da casa da " . $pessoa3[0][0][0][0][2][2];
+    echo("</br>");
+    echo " O " . $pessoa4[0][0][0][0][0][0] . " tem " . $pessoa4[0][0][0][0][1][1] . " e é da casa da " . $pessoa4[0][0][0][0][2][2];
+    ?>
